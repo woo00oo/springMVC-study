fde06f18ffb63f6d7cd362ab9a95b0340819c58b
woo00oo <96woo94@naver.com>
2021-06-07 12:40:18 +0900

    서블릿으로 회원 관리 웹 애플리케이션 만들기

---
+++ src/main/java/hello/servlet/web/servlet/MemberListServlet.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.servlet;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.List;
+
+@WebServlet(name = "memberListServlet", urlPatterns = "/servlet/members")
+public class MemberListServlet extends HttpServlet {
+
+    private MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.setCharacterEncoding("utf-8");
+
+        List<Member> members = memberRepository.findAll();
+        PrintWriter w = response.getWriter();
+
+       //html 코드 생략
+    }
+}
