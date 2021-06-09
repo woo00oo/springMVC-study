d24a0d5971541680476cd6d34be9b45e5c2e4bdb
woo00oo <96woo94@naver.com>
2021-06-09 15:33:27 +0900

    Model 추가 - V3

---
+++ src/main/java/hello/servlet/web/frontcontroller/ModelView.java
@@ -0,0 +1,30 @@
+package hello.servlet.web.frontcontroller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ModelView {
+
+    private String ViewName;
+    private Map<String, Object> model = new HashMap<>();
+
+    public ModelView(String viewName) {
+        ViewName = viewName;
+    }
+
+    public String getViewName() {
+        return ViewName;
+    }
+
+    public void setViewName(String viewName) {
+        ViewName = viewName;
+    }
+
+    public Map<String, Object> getModel() {
+        return model;
+    }
+
+    public void setModel(Map<String, Object> model) {
+        this.model = model;
+    }
+}
